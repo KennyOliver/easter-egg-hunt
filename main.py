1dd20e656e96324af715e69523ec938daa23f67c
Kenny <kenny_oliver@icloud.com>
2021-03-14 09:33:49 +0000

    [+] Rearranged some code just for maintainability [+] Changed variable number to egg_count in find_eggs() [+] Changed path_size to path_length [+] Changed parameter size in subprograms to length

--- main.py
+++ main.py
@@ -2,55 +2,54 @@ import random
 import time          
 from vividHues import Clr #my own module for coloured strings!
 #====================
-def generate_path(size):
+def generate_path(length):
   """ create path and fill with "s" """
-  for item in range(size):
+  for item in range(length):
     path.append("s")
   #print(path)
 #====================
-def hide_eggs(size,number_eggs):
+def hide_eggs(length,number_eggs):
   """ hide eggs at random locations on path """
   for item in range(number_eggs):
-    egg_pos = random.randint(0,size-1)
+    egg_pos = random.randint(0,length-1)
     #print(egg_pos, end=",")
     path[egg_pos] = "E"
   #print(path)
 #====================
-def find_eggs(size,hop):
+def find_eggs(length,hop):
   bunny_pos = 0 #starts on 1st stone of path
-  number = 0
+  egg_count = 0
   
-  while (bunny_pos + hop) <= (size-1):
+  while (bunny_pos + hop) <= (length-1):
     bunny_pos += hop
     print("hop...", end = "")
     time.sleep(random.randint(1,3)/8)
   
     if path[bunny_pos] == "E":
-      number += 1
+      egg_count += 1
       print(Clr.LIME + "Egg found!" + Clr.RESET)
   
-  print(Clr.GREEN + f"\n{bunny_name} has found {number} egg(s)!" + Clr.RESET)
+  print(Clr.GREEN + f"\n{bunny_name} has found {egg_count} egg(s)!" + Clr.RESET)
 #====================
 # MAIN PROGRAM
 path = []
-path_size = 50
+path_length = 50
 number_eggs = 20
 
-generate_path(path_size)
-
 # random.seed(10)
-hide_eggs(path_size,number_eggs)
+generate_path(path_length)
+hide_eggs(path_length,number_eggs)
+hop_size = random.randint(1,5)
 
 print("<-- Easter Egg Hunt -->")
 print(Clr.GREEN + "Welcome to the annual Easter Egg hunt!" + Clr.RESET)
-print(Clr.LIME + f"Your Easter Bunny will look for {number_eggs} Easter Eggs hidden along a {path_size}m path")
+print(Clr.LIME + f"Your Easter Bunny will look for {number_eggs} Easter Eggs hidden along a {path_length}m path")
 
 print(Clr.BLACK + "-" * 30 + Clr.RESET)
 bunny_name = input(Clr.YELLOW + "Enter a name for your Easter Bunny\n\t--> " + Clr.RESET)
 print(Clr.BLACK + "-" * 30 + Clr.RESET)
 
-hop_size = random.randint(1,5)
 print(Clr.YELLOW + f"{bunny_name} can hop {hop_size}m")
 print(Clr.PINK + f"How many eggs will {bunny_name} find along the path?\n" + Clr.RESET)
 
-find_eggs(path_size,hop_size)
+find_eggs(path_length,hop_size)
